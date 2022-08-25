6f506e34a57560c2e8a8057caff66dc947b0aafc
Ibrahim Aliyev <ibrahimma@code.edu.az>
2022-08-25 18:10:29 +0400

    Task1

---
+++ Task1-25.08.2022/Models/MyList.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task1_25._08._2022.Models
+{
+    class MyList<T>
+    {
+        private int _capacity;
+        public int Capacity { get { return _capacity; } set { if (value > 0) _capacity = value; } }
+        private int _count;
+        public int Count { get { return _count; } }
+        private T[] _arr;
+        public T this[int index]
+        {
+            get
+            {
+                if (index < Count) return _arr[index];
+                throw new IndexOutOfRangeException();
+            }
+            set
+            {
+                if (index < Count)
+                {
+                    _arr[index] = value;
+                    return;
+                }
+                throw new IndexOutOfRangeException();
+            }
+        }
+        public MyList()
+        {
+            Capacity = 1;
+            _arr = new T[Capacity];
+        }
+        public MyList(int Capacity)
+        {
+            this.Capacity = Capacity;
+            _arr = new T[Capacity];
+        }
+
+        public void Add(T value)
+        {
+
+            if (Count == _arr.Length)
+            {
+                Array.Resize(ref _arr, _arr.Length + Capacity);
+            }
+            _count++;
+            _arr[Count - 1] = value;
+
+
+        }
+        public void Clear()
+        {
+            _arr = new T[0];
+            _count = 0;
+        }
+        public bool Exist(T value)
+        {
+            for (int i = 0; i < Count; i++)
+            {
+                if (_arr[i].Equals(value)) return true;
+            }
+            return false;
+        }
+        public bool Remove(T value)
+        {
+            for (int i = 0; i < Count; i++)
+            {
+                if (_arr[i].Equals(value))
+                {
+                    for (; i < Count - 1; i++)
+                    {
+                        _arr[i] = _arr[i + 1];
+                    }
+                    _count--;
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public void Reverse()
+        {
+            int n = Count - 1;
+            for (int i = 0; i < n; i++)
+            {
+                T temp = _arr[i];
+                _arr[i] = _arr[n];
+                _arr[n] = temp;
+                n--;
+            }
+        }
+
+        public int IndexOf(T value)
+        {
+            for (int i = 0; i < Count; i++)
+            {
+                if (_arr[i].Equals(value)) return i;
+            }
+            return -1;
+        }
+
+        public int LastIndexOf(T value)
+        {
+            for (int i = Count - 1; i >= 0; i--)
+            {
+                if (_arr[i].Equals(value)) return i;
+            }
+            return -1;
+        }
+
+    }
+}
---
+++ Task1-25.08.2022/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Task1_25._08._2022.Models;
+
+namespace Task1_25._08._2022
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList<int> list1 = new MyList<int>(5);
+            list1.Add(5);
+            list1.Add(6);
+            list1.Add(10);
+            list1.Add(8);
+            list1.Add(63);
+            list1.Reverse();
+            list1.Remove(10);
+            list1.Clear();
+            list1.Add(82);
+            list1.Add(9);
+            list1.Add(4);
+            list1.Add(140);
+            list1.Add(9);
+            for (int i = 0; i < list1.Count; i++)
+            {
+                Console.WriteLine("Value: "+ list1[i]);
+            }
+            Console.WriteLine("Exist: " + list1.Exist(56));
+            Console.WriteLine("IndexOf: "+ list1.IndexOf(9));
+            Console.WriteLine("LastIndexOf: " + list1.LastIndexOf(9));
+            
+
+        }
+    }
+    
+}
